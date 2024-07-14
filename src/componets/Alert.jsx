0bc3f85bb4d51a2b9e0a5941e61ca453ac4c8eb9
valeriy korovin <1k9o9r5@gmail.com>
2024-07-14 16:47:25 +0300

    HW81_v5

--- src/componets/Alert.jsx
+++ src/componets/Alert.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import cn from "classnames";
-import PropTypes from "prop-types";
 
 class Alert extends React.Component {
     render() {
@@ -13,13 +12,6 @@ class Alert extends React.Component {
             </div>
         )
     }
-}
-
-Alert.propTypes = {
-    text: PropTypes.string.isRequired,
-    type: PropTypes.oneOf([
-        'primary', 'secondary', 'success', 'danger', 'warning', 'info', 'light', 'dark'
-    ]).isRequired
 }
 
 export default Alert
