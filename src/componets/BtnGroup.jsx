cd8e3d7a1a737975a7be0f9f88a8cc1b350feb1c
valeriy korovin <1k9o9r5@gmail.com>
2024-07-16 17:20:05 +0300

    HW83_v4

--- src/componets/BtnGroup.jsx
+++ src/componets/BtnGroup.jsx
@@ -1,45 +1,88 @@
+// import React from "react";
+//
+// class BtnGroup extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             leftActive: false,
+//             rightActive: false
+//         };
+//     }
+//
+//     handleLeftClick = () =>{
+//         this.setState({
+//             leftActive: true,
+//             rightActive: false
+//         });
+//     };
+//
+//     handleRightClick = () =>{
+//         this.setState({
+//             leftActive: false,
+//             rightActive: true
+//         });
+//     };
+//
+//     render() {
+//         const {leftActive, rightActive} = this.state;
+//
+//         return (
+//             <div className="btn-group" role="group">
+//                 <button
+//                     type="button"
+//                     className={`btn btn-secondary left ${leftActive ? 'active' : ''}`}
+//                     onClick={this.handleLeftClick}
+//                 >
+//                     LEFT
+//                 </button>
+//
+//                 <button
+//                     type="button"
+//                     className={`btn btn-secondary right ${rightActive ? 'active' : ''}`}
+//                     onClick={this.handleRightClick}
+//                 >
+//                     RIGHT
+//                 </button>
+//             </div>
+//         )
+//     }
+// }
+//
+// export default BtnGroup
+
 import React from "react";
 
 class BtnGroup extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            leftActive: false,
-            rightActive: false
+           activeBut: null
         };
     }
 
-    handleLeftClick = () =>{
+    handleButClick = (button) =>{
         this.setState({
-            leftActive: true,
-            rightActive: false
-        });
-    };
-
-    handleRightClick = () =>{
-        this.setState({
-            leftActive: false,
-            rightActive: true
+           activeBut: button
         });
     };
 
     render() {
-        const {leftActive, rightActive} = this.state;
+        const {activeBut} = this.state;
 
         return (
             <div className="btn-group" role="group">
                 <button
                     type="button"
-                    className={`btn btn-secondary left ${leftActive ? 'active' : ''}`}
-                    onClick={this.handleLeftClick}
+                    className={`btn btn-secondary left ${activeBut === 'left' ? 'active' : ''}`}
+                    onClick={() => this.handleButClick('left')}
                 >
                     LEFT
                 </button>
 
                 <button
                     type="button"
-                    className={`btn btn-secondary right ${rightActive ? 'active' : ''}`}
-                    onClick={this.handleRightClick}
+                    className={`btn btn-secondary right ${activeBut === 'right' ? 'active' : ''}`}
+                    onClick={() => this.handleButClick('right')}
                 >
                     RIGHT
                 </button>
