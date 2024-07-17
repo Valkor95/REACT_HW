fd2d594b364a2b7bb604aad4b333820336f2e9fb
valeriy korovin <1k9o9r5@gmail.com>
2024-07-17 17:35:28 +0300

    HW84_v5

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Carousel from "./componets/Carousel.jsx";
 import React from "react";
-import images from "./images"
+import firstImage from "./images/first.jpg"
+import secondImage from "./images/second.jpg"
+import thirdImage from "./images/third.jpg"
 
 class App extends React.Component{
     render() {
-
+        const images = [firstImage, secondImage, thirdImage]
 
         return (
           <Carousel images={images}/>
