dcab9c60b6552ea4c5078aaf04fe9379a7143707
valeriy korovin <1k9o9r5@gmail.com>
2024-07-17 16:58:01 +0300

    HW84_v1

--- src/componets/Carousel.jsx
+++ src/componets/Carousel.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-class Hello extends React.Component {
+class Carousel extends React.Component {
     render() {
         return <h1>Hello</h1>
     }
 }
 
-export default Hello
+export default Carousel
