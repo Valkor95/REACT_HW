30a73605792edf16c636604d3c7b0985dfab6ce6
valeriy korovin <1k9o9r5@gmail.com>
2024-07-18 14:41:30 +0300

    HW86_v2

--- src/componets/MyForm.jsx
+++ src/componets/MyForm.jsx
@@ -1,6 +1,19 @@
 import React from "react";
 
 class MyForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: '',
+            address: '',
+            city: '',
+            country: '',
+            acceptRules: false,
+            submitted: false,
+            formData: {},
+        };
+    }
     render() {
         return <h1>Hello</h1>
     }
