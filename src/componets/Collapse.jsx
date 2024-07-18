9e9714d9da9bce7a344306eb6419196a06943cb3
valeriy korovin <1k9o9r5@gmail.com>
2024-07-18 14:22:32 +0300

    HW85_v2

--- src/componets/Collapse.jsx
+++ src/componets/Collapse.jsx
@@ -1,14 +1,46 @@
 import React from "react";
+import cn from "classnames";
 
 class Collapse extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            isOpen: props.opened !== undefined
+            isOpen: props.opened !== undefined ? props.opened : true,
         }
     }
+
+    toggle = () => {
+        this.setState((prevState) => ({
+            isOpen: !prevState.isOpen
+        }))
+    }
     render() {
-        return <h1>Hello</h1>
+        const {isOpen} = this.state;
+        const {text} = this.props;
+
+        return (
+            <div>
+                <p>
+                    <a className="btn btn-primary"
+                       data-bs-toggle="collapse"
+                       href="#"
+                       role="button"
+                       aria-expanded={isOpen}
+                       onClick={(e) => {
+                           e.preventDefault();
+                           this.toggle();
+                       }}
+                    >
+                        Link with href
+                    </a>
+                </p>
+                <div className={cn('collapse', {show: isOpen})}>
+                    <div className="card card-body">
+                        {text}
+                    </div>
+                </div>
+            </div>
+        )
     }
 }
 
