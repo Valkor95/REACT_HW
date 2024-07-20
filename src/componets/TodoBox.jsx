4301e27002ae6d471d07a4948762895078273ff5
valeriy korovin <1k9o9r5@gmail.com>
2024-07-20 20:58:07 +0300

    HW88_v3

--- src/componets/TodoBox.jsx
+++ src/componets/TodoBox.jsx
@@ -1,6 +1,23 @@
 import React from "react";
+import Item from "./ChildComponents/Item.jsx";
+import {uniqueId} from "./ChildComponents/uniqueId.js";
 
 class TodoBox extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            tasks: [],
+            currentTask: ''
+        };
+
+
+    }
+
+    handleChange(event){
+        this.setState({currentTask: event.target.value});
+    }
+
+
     render() {
         return <h1>Hello</h1>
     }
