2f6f499e9f2c4035e70ee04f938e97b80a65ac6f
valeriy korovin <1k9o9r5@gmail.com>
2024-07-20 20:19:29 +0300

    HW87_v5

--- src/componets/Component.jsx
+++ src/componets/Component.jsx
@@ -1,15 +1,47 @@
 import React from "react";
+import LogControls from "./ChildComponents/LogControls.jsx";
+import LogEntry from "./ChildComponents/LogEntry.jsx";
 
 class Component extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            log:[]
+        }
+    }
+
+    handleAdd = () => {
+        this.setState(prevState => ({
+            log: [prevState.log.length > 0 ? prevState.log[0] + 1 : 1, ...prevState.log]
+        }))
+    };
+
+    handleSubtract = () => {
+        this.setState(prevState => ({
+            log: [prevState.log.length > 0 ? prevState.log[0] - 1 : -1, ...prevState.log]
+        }))
+    };
+
+    handleRemove = (index) => {
+        this.setState(prevState => ({
+            log: prevState.log.filter((_, i) => i !== index)
+        }));
+    };
     render() {
+        const {log} = this.state;
+
         return (
             <div>
-                <div className="btn-group font-monospace" role="group">
-                    <button type="button" className="btn btn-outline-success">+</button>
-                    <button type="button" className="btn btn-outline-danger">-</button>
-                </div>
+                <LogControls onAdd={this.handleAdd} onSubtract={this.handleSubtract} />
+                {log.length > 0 && (
+                    <div className="list-group">
+                        {log.map((value, index) => (
+                            <LogEntry key={index} value={value} onRemove={() => this.handleRemove(index)} />
+                        ))}
+                    </div>
+                )}
             </div>
-        )
+        );
     }
 }
 
