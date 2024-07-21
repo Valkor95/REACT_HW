c72875e27168ff9c3b9f88876540c14936be00ef
valeriy korovin <1k9o9r5@gmail.com>
2024-07-21 20:21:44 +0300

    HW89_v3

--- src/componets/Card.jsx
+++ src/componets/Card.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 
 const Card = () => {
-    return ({
+    return (
     <div className="card">
         <div className="card-body">
             <h4 className="card-title">Title</h4>
             <p className="card-text">Text</p>
         </div>
     </div>
-    })
+    )
 }
 
 export default Card
