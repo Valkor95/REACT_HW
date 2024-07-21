52931dc04f7dd66447f2ab1240feabd17f3338f9
valeriy korovin <1k9o9r5@gmail.com>
2024-07-22 00:03:48 +0300

    HW90_v11

--- src/App.jsx
+++ src/App.jsx
@@ -16,8 +16,15 @@ class App extends React.Component{
           <div>
               <button type="button" className="modal-open-button btn btn-danger" onClick={this.toggle}>Open</button>
               <Modal isOpen={this.state.modal}>
-                  <Modal.Header></Modal.Header>
-                  <Modal.Body/>
+                  <Modal.Header toggle={this.toggle}>Modal title</Modal.Header>
+                  <Modal.Body>
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit
+                  </Modal.Body>
+                  <Modal.Footer>
+                      <button type="button" className="modal-close-button btn btn-secondary"
+                              onClick={this.toggle}>Cancel
+                      </button>
+                  </Modal.Footer>
               </Modal>
           </div>
         )
--- src/componets/Child Components/Body.jsx
+++ src/componets/Child Components/Body.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const Body = ({children}) => (
     <div className="modal-body">
         {children}
