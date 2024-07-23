b829b308ca0128a128285f6470cae7f0b897652b
valeriy korovin <1k9o9r5@gmail.com>
2024-07-24 01:06:42 +0300

    HW88-version2_v13

--- src/Components/TodoForm/TodoForm.jsx
+++ src/Components/TodoForm/TodoForm.jsx
@@ -1,14 +1,27 @@
 import React, {Component} from 'react';
-import {Form} from "react-bootstrap";
+import {Button, Form} from "react-bootstrap";
 
 class TodoForm extends Component {
     render() {
         return (
             <Form>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Group className="mb-3" controlId="todoItemTitle">
                     <Form.Label>Title</Form.Label>
-                    <Form.Control type="text" placeholder="Enter todo item title" />
+                    <Form.Control
+                        name='title'
+                        type="text"
+                        placeholder="Enter todo item title" />
                 </Form.Group>
+
+                <Form.Group className="mb-3" controlId="todoItemDescription">
+                    <Form.Label>Description</Form.Label>
+                    <Form.Control
+                        name='description'
+                        as='textarea'
+                        type="text"
+                        placeholder="Enter todo item description" />
+                </Form.Group>
+                <Button type='Submit'>Submit</Button>
             </Form>
         );
     }
