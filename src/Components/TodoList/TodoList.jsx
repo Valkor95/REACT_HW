d46462f8fda73ae212724238deb5092f9548366c
valeriy korovin <1k9o9r5@gmail.com>
2024-07-23 23:47:11 +0300

    HW88-version2_v6

---
+++ src/Components/TodoList/TodoList.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class TodoList extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+TodoList.propTypes = {};
+
+export default TodoList;
