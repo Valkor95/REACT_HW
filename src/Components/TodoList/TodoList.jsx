0ac0457cfe058ca0e98bc7b4c971e2c05aa80326
valeriy korovin <1k9o9r5@gmail.com>
2024-07-24 00:54:19 +0300

    HW88-version2_v11

--- src/Components/TodoList/TodoList.jsx
+++ src/Components/TodoList/TodoList.jsx
@@ -6,10 +6,17 @@ class TodoList extends Component {
     render() {
         return (
             <div className="todo-list">
+                <div className='text-center'>
+                    <h1>Todo List</h1>
+                </div>
                 <Container>
                     <Row>
-                        <Col xs={6}>Lorem ipsum dolor sit amet.</Col>
-                        <Col xs={6}>Lorem ipsum dolor sit amet.</Col>
+                        <Col xs={6}>
+
+                        </Col>
+                        <Col xs={6}>
+
+                        </Col>
                     </Row>
                 </Container>
             </div>
