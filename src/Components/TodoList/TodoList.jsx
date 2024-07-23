4310980706f5405088908e1da654d4ff3d400767
valeriy korovin <1k9o9r5@gmail.com>
2024-07-24 00:51:56 +0300

    HW88-version2_v10

--- src/Components/TodoList/TodoList.jsx
+++ src/Components/TodoList/TodoList.jsx
@@ -1,11 +1,17 @@
 import React, {Component} from 'react';
+import {Col, Container, Row} from "react-bootstrap";
 import PropTypes from 'prop-types';
 
 class TodoList extends Component {
     render() {
         return (
             <div className="todo-list">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur, quos.
+                <Container>
+                    <Row>
+                        <Col xs={6}>Lorem ipsum dolor sit amet.</Col>
+                        <Col xs={6}>Lorem ipsum dolor sit amet.</Col>
+                    </Row>
+                </Container>
             </div>
         );
     }
