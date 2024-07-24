126ae16c43c235a12ce75af89ccfe9cabb0d088a
valeriy korovin <1k9o9r5@gmail.com>
2024-07-24 16:33:05 +0300

    HW88-version2_v20

--- src/Components/TodoItem/TodoItem.jsx
+++ src/Components/TodoItem/TodoItem.jsx
@@ -1,16 +1,32 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import {Card} from "react-bootstrap";
 
 class TodoItem extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
     render() {
+        const {title, body} = this.props
+
         return (
-            <div>
-                
-            </div>
+            <Card>
+                <Card.Body>
+                    <Card.Title>{title}</Card.Title>
+                    <Card.Text>
+                        {body}
+                    </Card.Text>
+                </Card.Body>
+            </Card>
         );
     }
 }
 
-TodoItem.propTypes = {};
+TodoItem.propTypes = {
+    title: PropTypes.string.isRequired,
+    body: PropTypes.string.isRequired
+};
 
 export default TodoItem;
