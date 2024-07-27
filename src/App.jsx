f31274841fa3b256c3bb843f9c5c042e25f3f3c9
valeriy korovin <1k9o9r5@gmail.com>
2024-07-28 00:40:41 +0300

    HW91_v2

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,31 @@ import React from "react";
 
 
 class App extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            usersPosts: []
+        }
+
+        this.fetchUsersPosts()
+    }
+
+    async fetchUsersPosts() {
+        const request = await fetch('https://jsonplaceholder.typicode.com/posts');
+        const data = await request.json();
+        const filtredData = data.map(({id, title, body}) => {
+            return {id, title, body}
+        })
+        this.setState({usersPosts: filtredData})
+    }
+
     render() {
         return (
-          <PostCatalog />
+          <PostCatalog
+              id={this.state.usersPosts.id}
+              title={this.state.usersPosts.title}
+              body={this.state.usersPosts.body}
+          />
         )
     }
 }
