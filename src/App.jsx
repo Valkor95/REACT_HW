07c286bc9d6780d32b8651c231c819ce4bd3c920
valeriy korovin <1k9o9r5@gmail.com>
2024-07-29 17:16:41 +0300

    HW92_v5

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import MarkdownEditor from "./componets/MarkdownEditor.jsx";
 import React from "react";
+import '@toast-ui/editor/dist/toastui-editor.css';
 
 
 class App extends React.Component{
     render() {
         return (
-          <MarkdownEditor />
+          <MarkdownEditor onContentChange={console.log}/>
         )
     }
 }
