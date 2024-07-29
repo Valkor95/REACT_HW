36f5851e7e69f9e69cb94c1000d8db99e5fa0d87
valeriy korovin <1k9o9r5@gmail.com>
2024-07-29 17:14:00 +0300

    HW92_v3

--- src/componets/MarkdownEditor.jsx
+++ src/componets/MarkdownEditor.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, {useEffect, useRef} from "react";
+import Editor from "@toast-ui/editor";
+import '@toast-ui/editor/dist/toastui-editor.css'
 
-class MarkdownEditor extends React.Component {
-    render() {
+const MarkdownEditor = ({onContentChange}) => {
+    const editorRef = useRef(null);
+    const editorInstanceRef = useRef(null);
+
+    useEffect(() => {
+        editorInstanceRef.current = new Editor({
+            el: editorRef.current,
+            hideModeSwitch: true,
+        })
+
+        editorInstanceRef.current.addHook('change', () =>{
+
+        })
+    }, []);
+
         return <h1>Hello</h1>
-    }
+
 }
 
 export default MarkdownEditor
