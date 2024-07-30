5a330a6b8bbe72cfbe48291af3ad4e676094629c
valeriy korovin <1k9o9r5@gmail.com>
2024-07-31 00:32:29 +0300

    HW94_v8

--- src/Components/TodoList/TodoList.jsx
+++ src/Components/TodoList/TodoList.jsx
@@ -3,48 +3,60 @@ import {Col, Container, Row} from "react-bootstrap";
 import TodoForm from "../TodoForm";
 import TodoItem from "../TodoItem";
 
-class TodoList extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            data: []
-        }
-    }
+const TodoList = () => {
 
-    handleSubmit = (todoItem) => {
-        this.setState({data: [...this.state.data, todoItem]})
-    }
-    render() {
-        return (
-            <div className="todo-list">
-                <div className='text-center'>
-                    <h1>Todo List</h1>
-                </div>
-                <Container>
-                    <Row>
-                        <Col xs={6}>
-                            <TodoForm onSubmit={this.handleSubmit}/>
-                        </Col>
-                        <Col xs={6}>
-                            <Row>
-                                {this.state.data.map(({title, description}, index) =>{
-                                    return (
-                                            <Col key={title + '' + index}
-                                                 data-id={title + '' + index}
-                                                 xs={4}
-                                                 className='mb-2'
-                                            >
-                                                <TodoItem title={title} body={description}/>
-                                            </Col>
-                                        )
-                                })}
-                            </Row>
-                        </Col>
-                    </Row>
-                </Container>
-            </div>
-        );
-    }
-}
+
+
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+
+// class TodoList extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             data: []
+//         }
+//     }
+//
+//     handleSubmit = (todoItem) => {
+//         this.setState({data: [...this.state.data, todoItem]})
+//     }
+//     render() {
+//         return (
+//             <div className="todo-list">
+//                 <div className='text-center'>
+//                     <h1>Todo List</h1>
+//                 </div>
+//                 <Container>
+//                     <Row>
+//                         <Col xs={6}>
+//                             <TodoForm onSubmit={this.handleSubmit}/>
+//                         </Col>
+//                         <Col xs={6}>
+//                             <Row>
+//                                 {this.state.data.map(({title, description}, index) =>{
+//                                     return (
+//                                             <Col key={title + '' + index}
+//                                                  data-id={title + '' + index}
+//                                                  xs={4}
+//                                                  className='mb-2'
+//                                             >
+//                                                 <TodoItem title={title} body={description}/>
+//                                             </Col>
+//                                         )
+//                                 })}
+//                             </Row>
+//                         </Col>
+//                     </Row>
+//                 </Container>
+//             </div>
+//         );
+//     }
+// }
 
 export default TodoList;
