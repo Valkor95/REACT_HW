ffd5e7304f8e9749c55042b5f64cf10146e86cd1
valeriy korovin <1k9o9r5@gmail.com>
2024-07-31 00:25:55 +0300

    HW94_v6

--- src/Components/TodoForm/TodoForm.jsx
+++ src/Components/TodoForm/TodoForm.jsx
@@ -3,20 +3,26 @@ import {Button, Form} from "react-bootstrap";
 import PropTypes from "prop-types";
 
 
-const TodoForm = () => {
-    const [formData, setFormData] = useState([{
+const TodoForm = ({onSubmit}) => {
+    const [formData, setFormData] = useState({
         title: '',
         description: ''
-    }]);
+    });
     const [isLoading, setIsLoading] = useState('false')
 
     const handleChange = ({target}) => {
-        const newFormData = {
+        setFormData({
             ...formData,
             [target.name]: target.value
-        }
+        })
 
-        setFormData(newFormData)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+
+
     }
 
     return (
@@ -32,22 +38,22 @@ const TodoForm = () => {
                 />
             </Form.Group>
 
-            {/*<Form.Group className="mb-3" controlId="todoItemDescription">*/}
-            {/*    <Form.Label>Description</Form.Label>*/}
-            {/*    <Form.Control*/}
-            {/*        name='description'*/}
-            {/*        as='textarea'*/}
-            {/*        type="text"*/}
-            {/*        placeholder="Enter todo item description"*/}
-            {/*        onChange={handleChange}*/}
-            {/*        value={formData.description}*/}
-            {/*    />*/}
-            {/*</Form.Group>*/}
-            {/*<Button type='Submit'>Submit</Button>*/}
+            <Form.Group className="mb-3" controlId="todoItemDescription">
+                <Form.Label>Description</Form.Label>
+                <Form.Control
+                    name='description'
+                    as='textarea'
+                    type="text"
+                    placeholder="Enter todo item description"
+                    onChange={handleChange}
+                    value={formData.description}
+                />
+            </Form.Group>
+            <Button type='Submit'>Submit</Button>
         </Form>
     );
 };
-
+//
 // class TodoForm extends Component {
 //     constructor(props) {
 //         super(props);
