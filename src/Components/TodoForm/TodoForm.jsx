7161627e6d767acafb24edd3e44d14103e6abbfa
valeriy korovin <1k9o9r5@gmail.com>
2024-07-31 00:30:24 +0300

    HW94_v7

--- src/Components/TodoForm/TodoForm.jsx
+++ src/Components/TodoForm/TodoForm.jsx
@@ -15,14 +15,19 @@ const TodoForm = ({onSubmit}) => {
             ...formData,
             [target.name]: target.value
         })
-
     }
 
     const handleSubmit = (e) => {
         e.preventDefault();
         e.stopPropagation();
 
+        for (const key in formData){
+            if (formData[key].trim() === ''){
+                return alert(`Empty ${key}`)
+            }
+        }
 
+        onSubmit({...formData})
     }
 
     return (
@@ -53,73 +58,6 @@ const TodoForm = ({onSubmit}) => {
         </Form>
     );
 };
-//
-// class TodoForm extends Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             formData: {
-//                 title: '',
-//                 description: '',
-//             },
-//             isLoading: false
-//         }
-//     }
-//
-//     handleChange = ({target}) => {
-//         const formData = {
-//             ...this.state.formData,
-//             [target.name]: target.value
-//         }
-//
-//         this.setState({formData})
-//     }
-//
-//     handleSubmit = (e) => {
-//         e.preventDefault();
-//         e.stopPropagation();
-//
-//         for(const key in this.state.formData){
-//             if(this.state.formData[key].trim() === ''){
-//                 return alert(`Empty ${key}`)
-//             }
-//         }
-//
-//         this.props.onSubmit({...this.state.formData})
-//
-//     }
-//     render() {
-//         const {title, description} = this.state;
-//
-//         return (
-//             <Form onSubmit={this.handleSubmit}>
-//                 <Form.Group className="mb-3" controlId="todoItemTitle">
-//                     <Form.Label>Title</Form.Label>
-//                     <Form.Control
-//                         name='title'
-//                         type="text"
-//                         placeholder="Enter todo item title"
-//                         onChange={this.handleChange}
-//                         value={title}
-//                     />
-//                 </Form.Group>
-//
-//                 <Form.Group className="mb-3" controlId="todoItemDescription">
-//                     <Form.Label>Description</Form.Label>
-//                     <Form.Control
-//                         name='description'
-//                         as='textarea'
-//                         type="text"
-//                         placeholder="Enter todo item description"
-//                         onChange={this.handleChange}
-//                         value={description}
-//                     />
-//                 </Form.Group>
-//                 <Button type='Submit'>Submit</Button>
-//             </Form>
-//         );
-//     }
-// }
 
 TodoForm.propTypes = {
     onSubmit: PropTypes.func.isRequired
