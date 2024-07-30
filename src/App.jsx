c7a6d31d8dc2ae63d6fd3d7fc85aeb31a48a0cbf
valeriy korovin <1k9o9r5@gmail.com>
2024-07-30 21:30:13 +0300

    HW93_v10

--- src/App.jsx
+++ src/App.jsx
@@ -29,34 +29,4 @@ const App = () => {
     );
 };
 
-
-// class App extends React.Component{
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             usersPosts: []
-//         }
-//
-//         this.fetchUsersPosts()
-//     }
-//
-//     async fetchUsersPosts() {
-//         const request = await fetch('https://jsonplaceholder.typicode.com/posts');
-//         const data = await request.json();
-//         const filtredData = data.map(({id, title, body}) => {
-//             return {id, title, body}
-//         })
-//         this.setState({usersPosts: filtredData})
-//     }
-//
-//     render() {
-//         return (
-//           <PostCatalog
-//               className='PostCatalog'
-//               data={this.state.usersPosts}
-//           />
-//         )
-//     }
-// }
-
 export default App
--- src/componets/PostCatalog.jsx
+++ src/componets/PostCatalog.jsx
@@ -1,45 +1,27 @@
-import React from "react";
-import pt from 'prop-types'
 import PropTypes from "prop-types";
 import styles from './styles.module.scss'
 
-
-const PostCatalog = () => {
+const PostCatalog = ({data}) => {
     return (
-        <div>
-
+        <div className={`posts ${styles.flex}`}>
+            <ul className={`posts__list ${styles.ul}`}>
+                {data.map((item) => {
+                    return (
+                        <li
+                            key={item.id + '|' + item.title[0]}
+                            className={`posts_single-post ${styles.li}`}
+                            data-post-id={item.id}
+                        >
+                            <h3 className='posts_post-title'>{item.id + ' | ' + item.title}</h3>
+                            <p className='posts_post-description'>{item.body}</p>
+                        </li>
+                    )
+                })}
+            </ul>
         </div>
     );
 };
 
-//
-// class PostCatalog extends React.Component {
-//     constructor(props) {
-//         super(props);
-//     }
-//
-//     render() {
-//         const {data} = this.props
-//         return (
-//                 <div className={`posts ${styles.flex}`}>
-//                     <ul className={`posts__list ${styles.ul}`}>
-//                         {data.map((item) => {
-//                             return (
-//                                 <li
-//                                     key={item.id + '|' + item.title[0]}
-//                                     className={`posts_single-post ${styles.li}`}
-//                                     data-post-id={item.id}
-//                                 >
-//                                     <h3 className='posts_post-title'>{item.id + ' | ' + item.title}</h3>
-//                                     <p className='posts_post-description'>{item.body}</p>
-//                                 </li>
-//                             )
-//                         })}
-//                     </ul>
-//                 </div>
-//         )
-//     }
-// }
 
 PostCatalog.propTypes = {
     data: PropTypes.array.isRequired
