2825ecd0de3dcb25c685e6b9a60af72724e5d055
valeriy korovin <1k9o9r5@gmail.com>
2024-08-03 23:36:58 +0300

    HW95_v9

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import TodoForm from "../../componets/TodoForm"
 import TodoList from "../../componets/TodoList"
 
-class Home extends React.Component {
-    render() {
-        return <h1>Hello</h1>
-    }
-}
+const Home = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
 
 export default Home
