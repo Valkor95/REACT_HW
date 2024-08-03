6d8afffecccf58c3f8a980988a824c859c283bbf
valeriy korovin <1k9o9r5@gmail.com>
2024-08-04 01:06:46 +0300

    HW95_v20

--- src/componets/TodoList/TodoList.jsx
+++ src/componets/TodoList/TodoList.jsx
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {Link} from 'react-router-dom';
+import styles from '../../styles/TodoList/TodoList.module.scss'
 
 const TodoList = () => {
+    const [todos, setTodos] = useState([]);
+
+    useEffect(() => {
+        const savedTodos = JSON.parse(localStorage.getItem('todos')) || [];
+        setTodos(savedTodos);
+    }, []);
+
+    const handleDelete = (id) => {
+      const updatedTodos = todos.filter(todo => todo.id !== id);
+      localStorage.setItem('todos', JSON.stringify(updatedTodos));
+      setTodos(updatedTodos);
+    };
+
     return (
         <div>
             
