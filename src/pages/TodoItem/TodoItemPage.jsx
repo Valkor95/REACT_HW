00a793efda7068170738fda04addf6e1fa963501
valeriy korovin <1k9o9r5@gmail.com>
2024-08-04 10:52:20 +0300

    HW95_v25

--- src/pages/TodoItem/TodoItemPage.jsx
+++ src/pages/TodoItem/TodoItemPage.jsx
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {useParams, useNavigate} from "react-router-dom";
+import {TextField, Button, Select, MenuItem} from "@mui/material";
+import styles from '../../styles/TodoItem/TodoItem.module.scss'
 
 const TodoItemPage = () => {
+    const {id} = useParams();
+    const navigate = useNavigate();
+    const [todo, setTodo] = useState(null);
+    const [editMode, setEditMode] = useState(false);
+
+    useEffect(() => {
+        const todos = JSON.parse(localStorage.getItem('todos')) || [];
+        const currentTodo = todos.find(t => t.id === Number(id));
+        if (currentTodo) setTodo(currentTodo);
+
+    }, []);
+
     return (
         <div>
 
