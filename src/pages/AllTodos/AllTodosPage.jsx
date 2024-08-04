d7032761e08ec239feb1ea990af70789255943fe
valeriy korovin <1k9o9r5@gmail.com>
2024-08-04 11:22:38 +0300

    HW95_v31

--- src/pages/AllTodos/AllTodosPage.jsx
+++ src/pages/AllTodos/AllTodosPage.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {Link} from 'react-router-dom';
+import styles from '../../styles/TodoList/TodoList.module.scss'
+import styles2 from '../../styles/TodoItem/TodoItem.module.scss'
 
 const AllTodosPage = () => {
+    const [todos, setTodos] = useState([]);
+
+    useEffect(() => {
+        const savedTodos = JSON.parse(localStorage.getItem('todos')) || [];
+        setTodos(savedTodos);
+    }, []);
+
     return (
-        <div>
-            
+        <div className={styles.todoList}>
+            {todos.map(todo => (
+                <div key={todo.id + todo.title} className={styles2.todoItem}>
+                    <h3>{todo.title}</h3>
+                    <p>{todo.description}</p>
+                    <p>Status: {todo.status}</p>
+                    <Link to={`/todo/${todo.id}`}>View</Link>
+                </div>
+            ))}
         </div>
     );
 };
