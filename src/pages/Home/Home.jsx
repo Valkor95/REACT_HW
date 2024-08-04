81d4febd83363cf0df884ee025a6f667b84a6494
valeriy korovin <1k9o9r5@gmail.com>
2024-08-04 11:39:02 +0300

    HW95_v35

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,12 +3,18 @@ import TodoForm from "../../componets/TodoForm"
 import TodoList from "../../componets/TodoList"
 
 const Home = () => {
+    const [update, setUpdate] = useState(0);
+
+    const handleAddTodo = () => {
+        setUpdate(update + 1);
+    }
+
     return (
         <div>
             <h1 style={{marginLeft: '60px', marginTop: '30px', textTransform: 'uppercase'}}
             >Todo List</h1>
-            <TodoForm/>
-            <TodoList/>
+            <TodoForm onAddTodo={handleAddTodo}/>
+            <TodoList key={update}/>
         </div>
     );
 };
