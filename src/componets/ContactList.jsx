00bfeef79b8fb8e4a370a4b8d7df9cb2a6058c75
valeriy korovin <1k9o9r5@gmail.com>
2024-08-14 12:49:33 +0300

    HW96_v7

--- src/componets/ContactList.jsx
+++ src/componets/ContactList.jsx
@@ -1,4 +1,10 @@
 import React from "react";
+import PropTypes from "prop-types";
+import {useSelector, useDispatch} from "react-redux";
+import {deleteContact} from "../store/slices/contact.js";
+import {Link} from "react-router-dom";
+import {Button, List, ListItem, ListItemText} from "@mui/material";
+import cn from 'classnames';
 
 class ContactList extends React.Component {
     render() {
