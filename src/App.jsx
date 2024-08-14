2ab0772da5eacf247c4376f11f689f2bec692802
valeriy korovin <1k9o9r5@gmail.com>
2024-08-14 13:36:55 +0300

    HW96_v18

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import ContactList from "./componets/ContactList.jsx";
+import ContactForm from "./componets/ContactForm.jsx";
 import React from "react";
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {Container} from "@mui/material";
 
 
-class App extends React.Component{
-    render() {
-        return (
-          <ContactList />
-        )
-    }
-}
+const App = () => {
+    return (
+        <Router>
+            <Container>
+                <Routes>
+                    <Route path='/' element={<ContactList/>}/>
+                    <Route path='/add' element={<ContactForm/>}/>
+                    <Route path='/edit/:id' element={<ContactForm/>}/>
+                </Routes>
+            </Container>
+        </Router>
+    );
+};
 
-export default App
+export default App;
