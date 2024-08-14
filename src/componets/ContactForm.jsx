9753417ec29f79fb4c13a9df35eded7063dbb1da
valeriy korovin <1k9o9r5@gmail.com>
2024-08-14 13:18:52 +0300

    HW96_v13

--- src/componets/ContactForm.jsx
+++ src/componets/ContactForm.jsx
@@ -1,6 +1,9 @@
 import React, {useEffect, useState} from 'react';
 import {useDispatch, useSelector} from "react-redux";
+import {addContact, updateContact} from "../store/slices/contact.js";
 import {useNavigate, useParams} from "react-router-dom";
+import {TextField, Button} from "@mui/material";
+import PropTypes from "prop-types";
 
 const ContactForm = () => {
     const [contact, setContact] = useState({name: '', email: ''});
