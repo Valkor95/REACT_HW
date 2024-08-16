d082c357e2bfc9a55ec195a16e9bc0085ad41306
valeriy korovin <1k9o9r5@gmail.com>
2024-08-16 23:15:37 +0300

    HW98_v2

---
+++ src/store/API_Slice/index.js
@@ -0,0 +1,17 @@
+import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
+import {API_MAIN_URL} from "../../url/index.js";
+
+export const postsAPI = createApi({
+    reducerPath: 'postsAPI',
+    baseQuery: fetchBaseQuery({
+        baseUrl: API_MAIN_URL
+    }),
+
+    endpoints(build) {
+        getPosts: build.query({
+            query: (arg) => `posts`
+        })
+
+
+    }
+})
