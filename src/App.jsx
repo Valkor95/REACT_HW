10a3d2b9079f0607843448cfe98f84f03c9b2434
valeriy korovin <1k9o9r5@gmail.com>
2024-08-16 23:19:27 +0300

    HW98_v4

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import Hello from "./componets/Hello.jsx";
+import ListData from "./componets/ListData.jsx";
 import React from "react";
 
 
 class App extends React.Component{
     render() {
         return (
-          <Hello />
+          <ListData />
         )
     }
 }
