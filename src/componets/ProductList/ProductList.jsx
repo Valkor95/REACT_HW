b00b330dc50891f58521a5b6113a48ce85ba890a
valeriy korovin <1k9o9r5@gmail.com>
2024-08-23 15:58:11 +0300

    FINAL_v11

--- src/componets/ProductList/ProductList.jsx
+++ src/componets/ProductList/ProductList.jsx
@@ -23,15 +23,15 @@ const ProductList = () => {
 
     return (
         <Grid container spacing={3} style={{display: 'flex', justifyContent: 'center', marginTop: '30px'}}>
-            {products.map(product => (
+            {filteredProducts.map(product => (
                 <Grid item
                       style={{display: 'flex', justifyContent: 'center'}}
                       xs={12} sm={6} md={4} key={product.id}>
                     <Card>
                         <img src={product.image} alt={product.name} style={{ width: '100%' }} />
                         <CardContent>
-                            <Typography variant="h6">{product.name}</Typography>
-                            <Typography variant="body1">{product.price}</Typography>
+                            <Typography variant="h6">{product.title}</Typography>
+                            <Typography variant="body1">{`$${product.price}`}</Typography>
                             <Button component={Link} to={`/product/${product.id}`}>View Details</Button>
                         </CardContent>
                     </Card>
