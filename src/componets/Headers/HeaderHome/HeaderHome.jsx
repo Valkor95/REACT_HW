1c3a713a09feb722592755f9e1d4493b18caaef2
valeriy korovin <1k9o9r5@gmail.com>
2024-08-26 16:52:16 +0300

    FINAL3_v5

--- src/componets/Headers/HeaderHome/HeaderHome.jsx
+++ src/componets/Headers/HeaderHome/HeaderHome.jsx
@@ -9,48 +9,14 @@ import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import {Col, Container, Row} from "react-bootstrap";
 import {Search} from "@mui/icons-material";
 import InputBase from '@mui/material/InputBase';
+import SearchStyle from "../../../styles/MUI/Search/index.js";
+
 const HeaderHome = () => {
-    const Search = styled('div')(({ theme }) => ({
-        position: 'relative',
-        borderRadius: theme.shape.borderRadius,
-        backgroundColor: alpha(theme.palette.common.black, 0.15),
-        '&:hover': {
-            backgroundColor: alpha(theme.palette.common.white, 0.25),
-        },
-        marginLeft: 0,
-        width: '100%',
-        [theme.breakpoints.up('sm')]: {
-            marginLeft: theme.spacing(1),
-            width: 'auto',
-        },
-    }));
+    const Search = styled('div')(({ theme }) => SearchStyle(theme).search);
 
-    const SearchIconWrapper = styled('div')(({ theme }) => ({
-        padding: theme.spacing(0, 2),
-        height: '100%',
-        position: 'absolute',
-        pointerEvents: 'none',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-    }));
+    const SearchIconWrapper = styled('div')(({ theme }) => SearchStyle(theme).searchIconWrapper);
 
-    const StyledInputBase = styled(InputBase)(({ theme }) => ({
-        color: 'inherit',
-        width: '100%',
-        '& .MuiInputBase-input': {
-            padding: theme.spacing(1, 1, 1, 0),
-            // vertical padding + font size from searchIcon
-            paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-            transition: theme.transitions.create('width'),
-            [theme.breakpoints.up('sm')]: {
-                width: '12ch',
-                '&:focus': {
-                    width: '20ch',
-                },
-            },
-        },
-    }));
+    const StyledInputBase = styled(InputBase)(({ theme }) => SearchStyle(theme).styledInputBase);
 
     return (
         <div className='header' style={{
