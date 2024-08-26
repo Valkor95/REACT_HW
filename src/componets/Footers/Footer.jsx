23782c3144d1d8d82be9963d6b4d963c302b160a
valeriy korovin <1k9o9r5@gmail.com>
2024-08-26 17:25:24 +0300

    FINAL3_v9

--- src/componets/Footers/Footer.jsx
+++ src/componets/Footers/Footer.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import {Col, Container, Row} from "react-bootstrap";
 
 const Footer = () => {
     return (
-        <div>
-            
+        <div className='footer' style={{
+            backgroundColor: 'rgba(0, 0, 0, 0.15)',
+        }}>
+            <Container>
+                <Row className="justify-content-between align-items-center">
+                    <Col xs="auto">
+                        Footer
+                    </Col>
+                </Row>
+            </Container>
         </div>
+
     );
 };
 
