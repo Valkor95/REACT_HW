c2994c4aaaf967bafb502c3f8480a02794ab4745
valeriy korovin <1k9o9r5@gmail.com>
2024-08-26 17:33:08 +0300

    FINAL3_v11

--- src/componets/Footers/Footer.jsx
+++ src/componets/Footers/Footer.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import {Col, Container, Row} from "react-bootstrap";
-
+import {Link} from "react-router-dom";
+import {ROUTES} from "../../utils/routes.js";
+import Logo from "../../img/Logo/Logo.png";
+import FacebookIcon from '@mui/icons-material/Facebook';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import YouTubeIcon from '@mui/icons-material/YouTube';
 const Footer = () => {
     return (
         <div className='footer' style={{
@@ -9,7 +14,27 @@ const Footer = () => {
             <Container>
                 <Row className="justify-content-between align-items-center">
                     <Col xs="auto">
+                        <div className="logo-container" style={{ maxWidth: '50px' }}>
+                            <Link to={ROUTES.HOME}>
+                                <img src={Logo} alt="logo" style={{ width: '100%', height: 'auto', display: 'block' }} />
+                            </Link>
+                        </div>
+                    </Col>
+
+                    <Col xs="auto">
                         Footer
+                    </Col>
+                </Row>
+                <Row className="justify-content-between align-items-center">
+                    <Col xs="auto" className='rights'>
+                        Developed by Hillel's Student Valeriy Korovin
+                    </Col>
+                </Row>
+                <Row className="justify-content-center align-items-center">
+                    <Col xs="auto" className='socials'>
+                        <FacebookIcon/>
+                        <LinkedInIcon/>
+                        <YouTubeIcon/>
                     </Col>
                 </Row>
             </Container>
