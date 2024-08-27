780db29860742d6511e5571b15c32e3326f5393e
valeriy korovin <1k9o9r5@gmail.com>
2024-08-27 17:29:25 +0300

    FINAL3_v19

--- src/componets/Sidebar/Sidebar.jsx
+++ src/componets/Sidebar/Sidebar.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import {List, ListItemButton, ListItemIcon, ListItemText, ListSubheader} from "@mui/material";
 
 const Sidebar = () => {
     return (
-        <div>
-            
-        </div>
+        <List
+            sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
+            component="nav"
+            aria-labelledby="nested-list-subheader"
+            subheader={
+                <ListSubheader component="div" id="nested-list-subheader">
+                    Categories
+                </ListSubheader>
+            }
+        >
+            <ListItemButton>
+                <ListItemText primary="Sent mail" />
+            </ListItemButton>
+        </List>
     );
 };
 
