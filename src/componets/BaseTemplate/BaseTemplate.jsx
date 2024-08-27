d0324f9ce872cb881f790cf155c644bcf4cafa03
valeriy korovin <1k9o9r5@gmail.com>
2024-08-27 16:13:16 +0300

    FINAL3_v15

---
+++ src/componets/BaseTemplate/BaseTemplate.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from "prop-types";
+import {Container} from "react-bootstrap";
+import {Box} from "@mui/material";
+
+const BaseTemplate = ({children, maxWidth = 'sm'}) => {
+    return (
+        <Container component="main"  maxWidth="lg">
+            <Box
+                sx={{
+                    marginTop: 8,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}
+            >
+                <Container maxWidth={maxWidth}>{children}</Container>
+            </Box>
+        </Container>
+    );
+};
+
+BaseTemplate.propTypes = {
+    children: PropTypes.node.isRequired,
+    maxWidth: PropTypes.oneOf(['xs', 'sm', 'md', 'lg', 'xl'])
+}
+
+export default BaseTemplate;
