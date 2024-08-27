4d818aba07a7c34767bb6541592846d7d7a35874
valeriy korovin <1k9o9r5@gmail.com>
2024-08-27 16:19:33 +0300

    FINAL3_v16

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,22 @@ import AppRoutes from "./componets/ROUTES/AppRoutes.jsx";
 import HeaderHome from "./componets/Headers/HeaderHome/HeaderHome.jsx";
 import Footer from "./componets/Footers/Footer.jsx";
 import Sidebar from "./componets/Sidebar/Sidebar.jsx";
+import BaseTemplate from "./componets/BaseTemplate/BaseTemplate.jsx";
 
 const App = () => {
     return (
         <div className='app'>
-            <HeaderHome/>
+            <BaseTemplate maxWidth='xl'>
+                <HeaderHome/>
 
-            <div className='container'>
-                <Sidebar/>
-                <AppRoutes/>
-            </div>
+                <div className='container'>
+                    <Sidebar/>
+                    <AppRoutes/>
+                </div>
 
 
-            <Footer/>
+                <Footer/>
+            </BaseTemplate>
         </div>
     );
 };
