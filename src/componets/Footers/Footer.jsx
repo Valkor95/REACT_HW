c6ab16b2ecef2421cd14b61c3ce01b42ed6f841c
valeriy korovin <1k9o9r5@gmail.com>
2024-08-27 15:54:08 +0300

    FINAL3_v14

--- src/componets/Footers/Footer.jsx
+++ src/componets/Footers/Footer.jsx
@@ -6,14 +6,15 @@ import Logo from "../../img/Logo/Logo.png";
 import FacebookIcon from '@mui/icons-material/Facebook';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import YouTubeIcon from '@mui/icons-material/YouTube';
+import {Typography} from "@mui/material";
 const Footer = () => {
     return (
         <div className='footer' style={{
             backgroundColor: 'rgba(0, 0, 0, 0.1)',
-            padding: '30px'
+            padding: '10px'
         }}>
             <Container >
-                <Row className="justify-content-center align-items-center mb-3">
+                <Row className="justify-content-between align-items-center">
                     <Col xs="auto">
                         <div className="logo-container" style={{ maxWidth: '50px' }}>
                             <Link to={ROUTES.HOME}>
@@ -21,20 +22,15 @@ const Footer = () => {
                             </Link>
                         </div>
                     </Col>
-
-                </Row>
-                <Row className="justify-content-center align-items-center mb-3">
                     <Col xs="auto" className='rights'>
-                        Developed by Hillel's Student Valeriy Korovin
+                        <Typography variant="body1">Developed by Hillel's Student Valeriy Korovin</Typography>
                     </Col>
-                </Row>
-                <Row className="justify-content-center align-items-center mb-3">
                     <Col xs="auto" className='socials' style={{display: 'flex',
-                        alignItems: 'center',
-                        gap: '10px'}}>
-                        <FacebookIcon/>
-                        <LinkedInIcon/>
-                        <YouTubeIcon/>
+                            alignItems: 'center',
+                            gap: '10px'}}>
+                            <FacebookIcon/>
+                            <LinkedInIcon/>
+                            <YouTubeIcon/>
                     </Col>
                 </Row>
             </Container>
