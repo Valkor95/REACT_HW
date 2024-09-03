109331a663bf73b09ad2028fe1e8cbc0b1581e36
valeriy korovin <1k9o9r5@gmail.com>
2024-09-03 15:16:19 +0300

    FINAL3_v46

---
+++ src/componets/CartWindow/CartWindow.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Box} from "@mui/material";
+
+const CartWindow = () => {
+    return (
+        <Box component="section" sx={{ p: 5, border: '1px solid grey' }}>
+            This Box renders as an HTML section element.
+        </Box>
+    );
+};
+
+export default CartWindow;
