37c370d1e2f01a16a2104d2226dc2d52a01684c5
valeriy korovin <1k9o9r5@gmail.com>
2024-09-07 11:44:21 +0300

    FINAL3_v55

--- src/componets/CartWindow/CartWindow.jsx
+++ src/componets/CartWindow/CartWindow.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Box, Grid, Paper, Typography } from "@mui/material";
+import {Box, Button, Grid, IconButton, Paper, Typography} from "@mui/material";
 import { styled } from "@mui/material/styles";
 import { useGetProductsByIdQuery } from "../../store/API/slices/fakeStoreApi.js";
 import {useSelector, useDispatch} from "react-redux";
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
 import DeleteIcon from '@mui/icons-material/Delete';
+import ShoppingCartCheckoutIcon from '@mui/icons-material/ShoppingCartCheckout';
 import { incrementQuantity, decrementQuantity, removeFromCart, clearCart } from '../../store/slices/cartCount.js'
 
 const Item = styled(Paper)(({ theme }) => ({
@@ -29,30 +30,59 @@ const CartWindow = () => {
                 {cartItems.map((cartItem) => {
                     const { data: product, isLoading, error } = useGetProductsByIdQuery(cartItem.id);
 
-                    if (isLoading) return <Typography key={cartItem.id}>Загрузка...</Typography>;
-                    if (error) return <Typography key={cartItem.id}>Ошибка при загрузке продукта</Typography>;
+                    if (isLoading) return <Typography key={cartItem.id}>Loading...</Typography>;
+                    if (error) return <Typography key={cartItem.id}>Something is wrong! Try again!</Typography>;
 
                     return (
-                        <Grid container key={cartItem.id} spacing={1} sx={{ marginBottom: '10px' }}>
-                            <Grid item xs={6}>
-                                <Item>
-                                    <Typography variant="h6">{product.title}</Typography>
-                                </Item>
+                        <Item sx={{marginBottom: 2}} key={cartItem.id}>
+                            <Grid container spacing={1} alignItems="center" sx={{ marginBottom: '10px' }}>
+                                <Grid item xs={1}>
+                                    <img src={product.image} alt={product.title}
+                                         style={{maxWidth: '100%', height: 'auto'}}/>
+                                </Grid>
+                                <Grid item xs={5}>
+                                        <Typography variant="h6">{product.title}</Typography>
+                                </Grid>
+                                <Grid item xs={3}>
+                                        <Typography>Количество: {cartItem.quantity}</Typography>
+                                        <IconButton onClick={() => dispatch(incrementQuantity({ id: cartItem.id }))}>
+                                            <AddIcon />
+                                        </IconButton>
+                                        <IconButton onClick={() => dispatch(decrementQuantity({ id: cartItem.id }))}>
+                                            <RemoveIcon />
+                                        </IconButton>
+                                </Grid>
+                                <Grid item xs={2}>
+                                        <Typography>Цена: ${product.price*cartItem.quantity}</Typography>
+                                </Grid>
+                                <Grid item xs={1}>
+                                    <IconButton onClick={() => dispatch(removeFromCart({ id: cartItem.id }))}>
+                                        <DeleteIcon style={{ color: 'red' }} />
+                                    </IconButton>
+                                </Grid>
                             </Grid>
-                            <Grid item xs={3}>
-                                <Item>
-                                    <Typography>Количество: {cartItem.quantity}</Typography>
-                                </Item>
-                            </Grid>
-                            <Grid item xs={3}>
-                                <Item>
-                                    <Typography>Цена: ${product.price*cartItem.quantity}</Typography>
-                                </Item>
-                            </Grid>
-                        </Grid>
+                        </Item>
                     );
                 })}
             </Grid>
+            <Box sx={{ marginTop: 2 }}>
+                <Button
+                    sx={{ marginRight: 2 }}
+                    variant="contained"
+                    color="secondary"
+                    onClick={() => dispatch(clearCart())}
+                >
+                    Clear cart
+                </Button>
+                <Button
+                    variant="contained"
+                    color="success"
+                    onClick={() => dispatch(clearCart())}
+                >
+                    Checkout
+                    <ShoppingCartCheckoutIcon style={{ color: 'white', marginLeft: 10}} />
+                </Button>
+            </Box>
         </Box>
     );
 };
