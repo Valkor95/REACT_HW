e1d1209f8a958e82f8aec28d739c9d9427e2f82f
valeriy korovin <1k9o9r5@gmail.com>
2024-09-11 11:33:28 +0300

    FINAL3_v67

--- src/componets/ProductList/CardPage.jsx
+++ src/componets/ProductList/CardPage.jsx
@@ -1,15 +1,23 @@
 import React, {useEffect} from 'react';
 import {useParams} from "react-router-dom";
 import {useGetProductByIdQuery} from "../../store/API/slices/fakeStoreApi.js";
+import {useDispatch} from "react-redux";
+import {addToCart} from "../../store/slices/cartCount.js";
+import LoadingIndicator from "./LoadingIndicator.jsx";
+import NoProductsMessage from "./NoProductsMesssage.jsx";
 
 const CardPage = () => {
     const {id} = useParams();
+    const dispatch = useDispatch();
 
-    useEffect(() => {
-        if(id){
-            useGetProductByIdQuery(id)
-        }
-    }, []);
+    const {data:product, isLoading, isError} = useGetProductByIdQuery()
+
+    const handleAddToCart = () => {
+        dispatch(addToCart({id: product.id}));
+    };
+
+    if(isLoading) return <LoadingIndicator/>
+    if (isError)  return <NoProductsMessage/>;
 
     return (
         <div>
