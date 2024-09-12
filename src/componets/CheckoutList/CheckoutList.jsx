7b255327ea8f3f245d8de0a326f308cadb43f354
valeriy korovin <1k9o9r5@gmail.com>
2024-09-12 23:17:59 +0300

    FINAL3_v76

--- src/componets/CheckoutList/CheckoutList.jsx
+++ src/componets/CheckoutList/CheckoutList.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
-import { Grid, TextField, Button, Typography, Box, FormControl, MenuItem, Select, InputLabel, Checkbox, FormControlLabel } from '@mui/material';
+import React, {useEffect, useState} from 'react';
+import {
+    Grid,
+    TextField,
+    Button,
+    Typography,
+    Box,
+    FormControl,
+    MenuItem,
+    Select,
+    InputLabel,
+    Checkbox,
+    FormControlLabel,
+    FormLabel, RadioGroup, Radio
+} from '@mui/material';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
 
 const cityOptions = ['Київ', 'Одеса', 'Харків', 'Львів', 'Кривий Ріг'];
 
 const CheckoutList = () => {
+    const [warehouses, setWarehouses] = useState([]);
+    const [deliveryMethod, setDeliveryMethod] = useState('pickup');
     // Инициализация Formik
     const formik = useFormik({
         initialValues: {
@@ -14,6 +29,9 @@ const CheckoutList = () => {
             firstName: '',
             patronymic: '',
             agree: false,
+            deliveryDate: '',
+            warehouse: '',
+            paymentMethod: 'cashOnDelivery',
         },
         validationSchema: Yup.object({
             city: Yup.string().required('Выберите город'),
@@ -24,6 +42,36 @@ const CheckoutList = () => {
             // Логика отправки данных формы
         },
     });
+
+    useEffect(() => {
+        const fetchWarehouses = async () => {
+            if (formik.values.city) {
+                try {
+                    const response = await fetch('https://api.novaposhta.ua/v2.0/json/', {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify({
+                            apiKey: 'f5f0a3aef570577e582a6fb867757e8f',
+                            modelName: 'Address',
+                            calledMethod: 'getWarehouses',
+                            methodProperties: {
+                                CityName: formik.values.city,
+                            },
+                        }),
+                    });
+                    const data = await response.json();
+                    setWarehouses(data.data);
+                } catch (error) {
+                    console.error('Error fetching warehouses:', error);
+                }
+            }
+        };
+
+        fetchWarehouses();
+    }, [formik.values.city]);
+
 
     return (
         <form onSubmit={formik.handleSubmit}>
@@ -97,6 +145,71 @@ const CheckoutList = () => {
                                 />
                             </Grid>
                         </Grid>
+                    </Box>
+
+                    <Box sx={{ mb: 4 }}>
+                        <Typography variant="h6">Выбор способа доставки</Typography>
+                        <FormControl component="fieldset">
+                            <FormLabel component="legend">Способ доставки</FormLabel>
+                            <RadioGroup
+                                name="deliveryMethod"
+                                value={deliveryMethod}
+                                onChange={(e) => setDeliveryMethod(e.target.value)}
+                            >
+                                <FormControlLabel value="pickup" control={<Radio />} label="Самовывоз" />
+                                <FormControlLabel value="novaPoshta" control={<Radio />} label="Новая Почта" />
+                            </RadioGroup>
+                        </FormControl>
+
+                        {deliveryMethod === 'novaPoshta' && (
+                            <>
+                                <TextField
+                                    select
+                                    fullWidth
+                                    id="warehouse"
+                                    name="warehouse"
+                                    label="Номер отделения"
+                                    value={formik.values.warehouse}
+                                    onChange={formik.handleChange}
+                                    margin="normal"
+                                >
+                                    {warehouses.map((warehouse) => (
+                                        <MenuItem key={warehouse.SiteKey} value={warehouse.Description}>
+                                            {warehouse.Description}
+                                        </MenuItem>
+                                    ))}
+                                </TextField>
+
+                                <TextField
+                                    fullWidth
+                                    id="deliveryDate"
+                                    name="deliveryDate"
+                                    label="Дата доставки"
+                                    type="date"
+                                    InputLabelProps={{
+                                        shrink: true,
+                                    }}
+                                    value={formik.values.deliveryDate}
+                                    onChange={formik.handleChange}
+                                    margin="normal"
+                                />
+                            </>
+                        )}
+                    </Box>
+
+                    <Box sx={{ mb: 4 }}>
+                        <Typography variant="h6">Выбор способа оплаты</Typography>
+                        <FormControl component="fieldset">
+                            <FormLabel component="legend">Способ оплаты</FormLabel>
+                            <RadioGroup
+                                name="paymentMethod"
+                                value={formik.values.paymentMethod}
+                                onChange={formik.handleChange}
+                            >
+                                <FormControlLabel value="cashOnDelivery" control={<Radio />} label="Оплата при получении" />
+                                <FormControlLabel value="onlinePayment" control={<Radio />} label="Онлайн оплата" />
+                            </RadioGroup>
+                        </FormControl>
                     </Box>
                 </Grid>
 
