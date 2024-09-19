fd8445ff0f23c4c2687102fea120812e84957ccc
valeriy korovin <1k9o9r5@gmail.com>
2024-09-19 13:36:35 +0300

    FINAL3_v92

--- src/componets/CheckoutList/CheckoutList.jsx
+++ src/componets/CheckoutList/CheckoutList.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Grid, Typography } from '@mui/material';
+import {Button, Checkbox, FormControlLabel, Grid, Typography} from '@mui/material';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import { useNavigate } from 'react-router-dom';
