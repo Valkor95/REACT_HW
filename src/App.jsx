0c189fc6e13bc3ac6b3360ea7f989894c9c3d155
valeriy korovin <1k9o9r5@gmail.com>
2024-10-05 18:48:34 +0300

    FINAL2_v10_'add Header-Footer-Sidebar to App'

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import RoutesComponent from "./ROUTES/RoutesComponent.jsx";
 import {BrowserRouter} from "react-router-dom";
+import Header from "./componets/Header/Header.jsx";
+import Footer from "./componets/Footer/Footer.jsx";
+import Sidebar from "./componets/Sidebar/Sidebar.jsx";
 
 const App = () => {
     return (
         <div className='App'>
-            <RoutesComponent/>
+            <Header/>
+            <div className='container'>
+                <Sidebar/>
+                <RoutesComponent/>
+            </div>
+            <Footer/>
         </div>
     );
 };
