03f61219d1c6e1de31c37ad782e1b91d50ebf674
valeriy korovin <1k9o9r5@gmail.com>
2024-10-05 18:56:02 +0300

    FINAL2_v11_'add Header logo'

--- src/componets/Header/Header.jsx
+++ src/componets/Header/Header.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import styles from '../../styles/Header.module.css'
+import {Link} from "react-router-dom";
+import routeNames from "../../ROUTES/routeNames.js";
+import Logo from "../../img/Logo.png"
+
 const Header = () => {
     return (
-        <div>
-            
+        <div className={styles.header}>
+            <div className={styles.logo}>
+                <Link to={routeNames.home}>
+                    <img src={Logo} alt="Logo"/>
+                </Link>
+            </div>
         </div>
     );
 };
